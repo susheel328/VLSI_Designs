b8de3c2f65bc80a395380f62aba6df5b7d48aa39
susheel328 <k21susheel@gmail.com>
2025-05-26 11:11:49 +0530

    Update upcounter.v

--- Up_counter/upcounter.v
+++ Up_counter/upcounter.v
@@ -1,11 +1,11 @@
-module up_counter(input clk, reset, output[3:0] counter
+module up_counter(input clk, rst, output[3:0] counter
     );
 reg [3:0] counter_up;
 
 // up counter
-always @(posedge clk or posedge reset)
+always @(posedge clk or posedge rst)
 begin
-if(reset)
+if(rst)
  counter_up <= 4'd0;
 else
  counter_up <= counter_up + 4'd1;
